81b12171cc2a0a47b6fd10d3ca0817db679b6ca4
qi xu <qi.xu@autronicafire.no>
2021-09-25 19:17:31 +0200

    loop

--- loop/main.go
+++ loop/main.go
@@ -4,14 +4,37 @@ import (
 	"fmt"
 )
 
-func condition() {
-	i := 0
-	for i < 5 {
+func condition() {	
+	for i := 0; i < 5; i++ {
 		fmt.Println(i)
+	}
+}
+
+func infinite() {	
+	i := 0
+	for {
 		i++
+		if i == 5{
+			break;
+		}
+		fmt.Println(i)
+	}
+}
+
+func collections() {
+	slice := []int{23,24,25,26,27}
+	for i, v := range slice {
+		fmt.Printf("index %d  value is %d \n", i, v )
+	}
+	fmt.Println("--------------")
+	myMap := map[int]string{1:"first", 2:"second", 3:"third"}
+	for i, v := range myMap {
+		fmt.Printf("index %d  value is %s \n", i, v )
 	}
 }
 
 func main() {
-	condition()
+	// condition();
+	// infinite();
+	collections();
 }
