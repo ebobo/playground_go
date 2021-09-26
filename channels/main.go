151eab2e5756466b4f94d3f8fc37fe2fa57688c6
qi xu <qi.xu@autronicafire.no>
2021-09-26 20:28:05 +0200

    channel

--- channels/main.go
+++ channels/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("test channel")
+
+	// The make() function, on the other hand, 
+	// is a special built-in function that is used to 
+	// initialize slices, maps, and channels. 
+	// Note that make() can only be used to initialize slices,
+	// maps, and channels, and that, unlike the new() function, make() does not return a pointer.
+
+	wg := &sync.WaitGroup{}
+	ch := make(chan int)
+
+	wg.Add(2)
+	//revice only channel
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		fmt.Println(<-ch)
+		wg.Done()
+	}(ch, wg)
+
+	//send only channel 
+	go func(ch chan<- int, wg *sync.WaitGroup) {
+		ch <- 29
+		wg.Done()
+	}(ch,wg)
+	
+	wg.Wait()
 }
