33ebba316d8a1f3f8515698e3895a27cf9c078f2
qi xu <qi.xu@autronicafire.no>
2022-01-11 15:51:59 +0100

    wip

--- channels/main.go
+++ channels/main.go
@@ -2,33 +2,83 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
+// func main() {
+// 	fmt.Println("test channel")
+
+// 	// The make() function, on the other hand,
+// 	// is a special built-in function that is used to
+// 	// initialize slices, maps, and channels.
+// 	// Note that make() can only be used to initialize slices,
+// 	// maps, and channels, and that, unlike the new() function, make() does not return a pointer.
+
+// 	wg := &sync.WaitGroup{}
+
+// 	// make a int channel
+// 	ch := make(chan int)
+
+// 	wg.Add(1)
+// 	//revice only channel
+// 	go func(ch <-chan int, wg *sync.WaitGroup) {
+// 		fmt.Println(<-ch)
+// 		wg.Done()
+// 	}(ch, wg)
+
+// 	//send only channel
+// 	go func(ch chan<- int, wg *sync.WaitGroup) {
+// 		ch <- 29
+// 		wg.Done()
+// 	}(ch, wg)
+
+// 	wg.Wait()
+// }
+
 func main() {
-	fmt.Println("test channel")
+	start := time.Now()
 
-	// The make() function, on the other hand, 
-	// is a special built-in function that is used to 
-	// initialize slices, maps, and channels. 
-	// Note that make() can only be used to initialize slices,
-	// maps, and channels, and that, unlike the new() function, make() does not return a pointer.
+	jobs := make(chan int, 50)
+	results := make(chan [2]int, 50)
 
-	wg := &sync.WaitGroup{}
-	ch := make(chan int)
+	go worker(jobs, results, 1)
+	go worker(jobs, results, 2)
+	go worker(jobs, results, 3)
+	go worker(jobs, results, 4)
+	go worker(jobs, results, 5)
+	go worker(jobs, results, 6)
+	go worker(jobs, results, 7)
+	go worker(jobs, results, 8)
+	go worker(jobs, results, 9)
+	go worker(jobs, results, 10)
 
-	wg.Add(2)
-	//revice only channel
-	go func(ch <-chan int, wg *sync.WaitGroup) {
-		fmt.Println(<-ch)
-		wg.Done()
-	}(ch, wg)
+	for i := 0; i < 50; i++ {
+		jobs <- i
+	}
 
-	//send only channel 
-	go func(ch chan<- int, wg *sync.WaitGroup) {
-		ch <- 29
-		wg.Done()
-	}(ch,wg)
-	
-	wg.Wait()
+	close(jobs)
+
+	for j := 0; j < 50; j++ {
+		result := <-results
+		fmt.Println(result[0], result[1])
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("app took %s", elapsed)
+
+	fmt.Println(" --- Done")
+}
+
+func worker(jobs <-chan int, results chan<- [2]int, workerid int) {
+	// pull number from jobs and call fib to get result
+
+	for n := range jobs {
+		results <- [2]int{workerid, fib(n)}
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
 }
