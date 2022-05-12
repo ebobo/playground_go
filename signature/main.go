694f93d2d43b45daf1960f66bbfddfbe043fb18b
qi xu <qi.xu@autronicafire.no>
2022-05-12 10:12:21 +0200

    wip

--- signature/main.go
+++ signature/main.go
@@ -10,24 +10,51 @@ import (
 
 func main() {
 	log.Println("Signature Go App")
-	f, err := os.Open("privateKey")
+
+	// get private key
+	prif, err := os.Open("privateKey")
 
 	if err != nil {
 		log.Println(err)
 	}
-	defer f.Close()
+	defer prif.Close()
 
-	privateKey, err := ioutil.ReadAll(f)
+	privateKey, err := ioutil.ReadAll(prif)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
+	// get public key
+	pubf, err := os.Open("publicKey")
+
+	if err != nil {
+		log.Println(err)
+	}
+	defer pubf.Close()
+
+	publicKey, err := ioutil.ReadAll(pubf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// signaturePayload
 	signaturePayload := []byte("Autronica")
 
+	// sign the payload
 	sign := ed25519.Sign(privateKey, signaturePayload[:])
 
-	sign = append(sign, signaturePayload...)
+	// verify the signature with publicKey, signaturePayload, sign
+	if !ed25519.Verify(publicKey, signaturePayload[:], sign) {
+		log.Println("can not verify")
+		return
+	} else {
+		log.Println("signature verified")
+	}
 
-	log.Printf("signature length: %d \nsignature: %v \n", len(sign), hex.EncodeToString(sign))
+	signPlusPayload := append(sign, signaturePayload...)
+
+	log.Printf("signature length: %d \nsignature: %v \n", len(signPlusPayload), hex.EncodeToString(signPlusPayload))
+
 }
